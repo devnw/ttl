e7a93a9129069f9fb62aeab9fbcdaa807e3ff886
github-action-benchmark <github@users.noreply.github.com>
2022-03-18 23:31:50 +0000

    add Benchmark Results (go) benchmark result for 13f7efc265fa948c44e4d76324b0af630b3f33ce

---
+++ dev/bench/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1647646310750,
+  "repoUrl": "https://github.com/devnw/ttl",
+  "entries": {
+    "Benchmark Results": [
+      {
+        "commit": {
+          "author": {
+            "email": "benji@devnw.com",
+            "name": "Benji Vesterby",
+            "username": "benjivesterby"
+          },
+          "committer": {
+            "email": "benji@devnw.com",
+            "name": "Benji Vesterby",
+            "username": "benjivesterby"
+          },
+          "distinct": true,
+          "id": "13f7efc265fa948c44e4d76324b0af630b3f33ce",
+          "message": "Merge branch 'go1.18'",
+          "timestamp": "2022-03-18T19:24:31-04:00",
+          "tree_id": "7747db595618002d7defc9e24d2e5b9ec27b316d",
+          "url": "https://github.com/devnw/ttl/commit/13f7efc265fa948c44e4d76324b0af630b3f33ce"
+        },
+        "date": 1647646310270,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "Benchmark_Set",
+            "value": 687.1,
+            "unit": "ns/op",
+            "extra": "1766768 times\n2 procs"
+          },
+          {
+            "name": "Benchmark_Get",
+            "value": 553.4,
+            "unit": "ns/op",
+            "extra": "2027007 times\n2 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
